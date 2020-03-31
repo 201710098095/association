0645d6961c523c472371c8d35807405172da1288
Yasar-l <liang965573557@qq.com>
2020-03-31 23:40:15 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,87 @@
+//index.js
+Page({
+  data: {
+    association:[
+      {"name":"工商协会",
+      "type":"学术",
+       "chargePerson":"李高",
+       "date":"2017-01-23",
+       "image":"../../img/shangxie.jpg",
+       "detail":{
+         "msg":"该社团由。。。。。。牛逼哄哄",
+         "activity":"校级活动。。。",
+         "recruit":"19日开始招募",
+         "sponsor":"没有赞助",
+         "other":"妇女节按时发不卡"
+        }
+      },
+
+       {"name":"乒乓球协会",
+       "type":"体育",
+       "chargePerson":"王小欧",
+       "date":"2018-05-12",
+       "image":"../../img/shangxie.jpg",
+       "detail":{
+        "msg":"sfdsdf",
+        "activity":"asdsda",
+        "recruit":"19日开始招募",
+        "sponsor":"没有赞助",
+        "other":"妇女节按时发不卡"
+       }
+      },
+
+       {"name":"街舞协会",
+       "type":"体育",
+       "chargePerson":"小明",
+       "date":"2015-12-03",       
+       "image":"../../img/shangxie.jpg",
+       "detail":{
+        "msg":"sfdsdf",
+        "activity":"asdsda",
+        "recruit":"19日开始招募",
+        "sponsor":"没有赞助",
+        "other":"妇女节按时发不卡"
+       }
+      },
+
+      {"name":"街舞协会",
+       "chargePerson":"小明",
+       "date":"2015-12-03",       
+       "image":"../../img/shangxie.jpg",
+       "detail":{
+        "msg":"sfdsdf",
+        "activity":"asdsda",
+        "recruit":"19日开始招募",
+        "sponsor":"没有赞助",
+        "other":"妇女节按时发不卡"
+       }
+      },
+      {"name":"街舞协会",
+      "chargePerson":"小明",
+      "date":"2015-12-03",       
+      "image":"../../img/shangxie.jpg",
+      "detail":{
+       "msg":"sfdsdf",
+       "activity":"asdsda",
+       "recruit":"19日开始招募",
+       "sponsor":"没有赞助",
+       "other":"妇女节按时发不卡"
+      }
+     },
+     {"name":"街舞协会",
+     "chargePerson":"小明",
+     "date":"2015-12-03",       
+     "image":"../../img/shangxie.jpg",
+     "detail":{
+      "msg":"sfdsdf",
+      "activity":"asdsda",
+      "recruit":"19日开始招募",
+      "sponsor":"没有赞助",
+      "other":"妇女节按时发不卡"
+     }
+    }
+      
+    ]
+  }
+
+})
